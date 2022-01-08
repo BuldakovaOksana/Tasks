78e87d08945383d31d99780c56eff07277c1531f
«Buldakova <79263143446@mail.ru>
2022-01-08 22:58:45 +0300

    first commit

---
+++ task000/Program.cs
@@ -0,0 +1,13 @@
+﻿//  0. Вывести квадрат числа
+
+
+int squareOfNumber(int number)
+{
+    int result = 0;
+    return result = number * number;
+}
+int number = 6;
+int result = squareOfNumber(number);
+System.Console.WriteLine($"квадрат числа {number} равен {result}");
+
+
